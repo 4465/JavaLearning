2875ece6730b05d0379f07cf8fe407d483242905
4465 <lwfrekach@gmail.com>
2022-03-11 00:30:24 +0800

    增加tree分类，增加二叉树的创建和相关题目

--- src/main/java/com/offer/tree/TreeNode.java
+++ src/main/java/com/offer/tree/TreeNode.java
@@ -4,6 +4,9 @@ public class TreeNode{
     public int val;
     public TreeNode left;
     public TreeNode right;
+
+    public TreeNode(){}
+
     public TreeNode(int val){
         this.val = val;
         this.left = null;
