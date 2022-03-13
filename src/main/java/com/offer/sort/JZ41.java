c60162e239d032e3261167a2f554d40853fad4fa
4465 <lwfrekach@gmail.com>
2022-03-14 00:04:51 +0800

    更新题解

--- src/main/java/com/offer/sort/JZ41.java
+++ src/main/java/com/offer/sort/JZ41.java
@@ -1,16 +1,27 @@
 package com.offer.sort;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Scanner;
 
-//堆排序求解
+
 public class JZ41 {
+    private ArrayList<Integer> arr = new ArrayList<>();
 
     public void Insert(Integer num) {
-
+        arr.add(num);
     }
 
     public Double GetMedian() {
-
+        Collections.sort(arr);
+        int len = arr.size();
+        if(len%2==0){
+            return ( arr.get(len/2) + arr.get(len/2 - 1) ) / 2.0;
+        }
+        else {
+            return 1.0 * arr.get(len / 2);
+        }
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/offer/str/JZ75.java
@@ -0,0 +1,7 @@
+package com.offer.str;
+
+import java.util.*;
+
+public class JZ75 {
+   
+}
