dc095405a73984c0af0a07759a622c89fc830e52
4465 <lwfrekcah@gmail.com>
2022-03-14 22:56:58 +0800

    更新题解

--- src/main/java/com/offer/sort/JZ41.java
+++ src/main/java/com/offer/sort/JZ41.java
@@ -5,13 +5,6 @@ import java.util.Scanner;
 //堆排序求解
 public class JZ41 {
 
-    public void Insert(Integer num) {
-
-    }
-
-    public Double GetMedian() {
-
-    }
 
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
