2f706211edbeaa99cd14d2887d3e4168e337b304
4465 <lwfrekcah@gmail.com>
2022-03-24 21:23:36 +0800

    增加携程笔试

---
+++ src/main/java/com/offer/sort/MySort.java
@@ -0,0 +1,41 @@
+package com.offer.sort;
+
+import java.util.Scanner;
+
+public class MySort {
+
+    public static void QuickSort(int[] arr, int l, int r){
+        if(l>=r)
+            return;
+
+        int key = arr[l];
+        int i = l;
+        int j = r;
+        while (i<j){
+            while (arr[j] > key && i < j){
+                j--;
+            }
+            arr[i] = arr[j];
+            while (arr[i] < key && i<j){
+                i++;
+            }
+            arr[j] = arr[i];
+        }
+        arr[i] = key;
+        QuickSort(arr, l, i-1);
+        QuickSort(arr, i+1, r);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        MySort.QuickSort(arr, 0, arr.length-1);
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
